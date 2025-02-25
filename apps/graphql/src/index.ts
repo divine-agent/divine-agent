36354fbb246f37720c38942a45c7eb386deac236
Kaikai <98205900+Kaikaikaifang@users.noreply.github.com>
2025-02-25 15:28:20 +0800

    chore(build): delete graphql deployment on vercel

--- apps/graphql/src/index.ts
+++ apps/graphql/src/index.ts
@@ -1,14 +1,14 @@
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
-import { typeDefs } from "./schema";
-import { resolvers } from "./resolvers";
-import { AuthAPI } from "./datasources/auth-api";
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { AuthAPI } from './datasources/auth-api';
+import { resolvers } from './resolvers';
+import { typeDefs } from './schema';
 
 async function startApolloServer() {
   const server = new ApolloServer({ typeDefs, resolvers });
   const { url } = await startStandaloneServer(server, {
     context: ({ req }) => {
-      const token = req.headers.authorization || "";
+      const token = req.headers.authorization || '';
       const { cache } = server;
       return Promise.resolve({
         dataSources: {

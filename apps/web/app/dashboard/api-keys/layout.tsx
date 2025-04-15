59a3ed3e3c44ca9dd9da5804330d7c2fdbedada6
kaikai <2837968358@qq.com>
2025-04-15 14:01:24 +0800

    fix(web): layout

--- apps/web/app/dashboard/api-keys/layout.tsx
+++ apps/web/app/dashboard/api-keys/layout.tsx
@@ -1,3 +1,4 @@
+import { ScrollArea } from '@workspace/ui/components/scroll-area';
 import { Separator } from '@workspace/ui/components/separator';
 import type React from 'react';
 import { CreateDialog } from './components/create-dialog';
@@ -8,19 +9,21 @@ interface APIKeysLayoutProps {
 
 export default function APIKeysLayout({ children }: APIKeysLayoutProps) {
   return (
-    <div className="space-y-3 py-3">
-      <div className="flex items-center justify-between px-6">
-        <h1 className=" text-xl tracking-tight">API Keys</h1>
-        <CreateDialog />
-      </div>
-      <Separator className="my-3" />
-      <div className="px-6">
-        <p className="text-muted-foreground text-sm">
-          As a owner of this account, you can view and manage all API keys in
-          this account.
-        </p>
-        <div className="mt-6">{children}</div>
-      </div>
+    <div className="h-full space-y-3 overflow-y-auto py-3">
+      <ScrollArea className="max-h-full">
+        <div className="flex items-center justify-between px-6">
+          <h1 className=" text-xl tracking-tight">API Keys</h1>
+          <CreateDialog />
+        </div>
+        <Separator className="my-3" />
+        <div className="px-6">
+          <p className="text-muted-foreground text-sm">
+            As a owner of this account, you can view and manage all API keys in
+            this account.
+          </p>
+          <div className="my-6">{children}</div>
+        </div>
+      </ScrollArea>
     </div>
   );
 }

9e68a3b398487fe7d349ebc3fe66c28470d6ba85
kaikai <2837968358@qq.com>
2025-04-15 13:54:11 +0800

    feat(web): trace overview layout

--- apps/web/app/dashboard/traces/(overview)/layout.tsx
+++ apps/web/app/dashboard/traces/(overview)/layout.tsx
@@ -1,3 +1,4 @@
+import { ScrollArea } from '@workspace/ui/components/scroll-area';
 import { Separator } from '@workspace/ui/components/separator';
 import type React from 'react';
 
@@ -7,17 +8,19 @@ interface TracesLayoutProps {
 
 export default function TracesLayout({ children }: TracesLayoutProps) {
   return (
-    <div className="space-y-3 py-3">
-      <div className="flex items-center justify-between px-6">
-        <h1 className=" text-xl tracking-tight">Trace</h1>
-      </div>
-      <Separator className="my-3" />
-      <div className="px-6">
-        <p className="text-muted-foreground text-sm">
-          You can view and manage all traces in this account.
-        </p>
-        <div className="mt-6">{children}</div>
-      </div>
+    <div className="h-full space-y-3 overflow-y-auto py-3">
+      <ScrollArea className="max-h-full">
+        <div className="flex items-center justify-between px-6">
+          <h1 className=" text-xl tracking-tight">Trace</h1>
+        </div>
+        <Separator className="my-3" />
+        <div className="px-6">
+          <p className="text-muted-foreground text-sm">
+            You can view and manage all traces in this account.
+          </p>
+          <div className="my-6">{children}</div>
+        </div>
+      </ScrollArea>
     </div>
   );
 }
